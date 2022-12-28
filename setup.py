15d39239828663ff76a7f3927ec89c7d79f90299
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-12-28 15:14:54 +0900

    setup.py: added py_modules

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(
     name="targs",
     version="0.0.1",
+    py_modules=['targs'],
     entry_points={
         "console_scripts": [
             "targs = targs:main"
