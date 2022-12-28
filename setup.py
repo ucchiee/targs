28b880c1c1f85eb6a132dbd0c7bcd31397fb38fe
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-12-28 19:11:35 +0900

    setup.py: added setuptools dependency

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from setuptools import setup
 
-
 setup(
     name="targs",
     version="0.0.1",
-    install_requires=['wheel'],
-    py_modules=['targs'],
-    entry_points={
-        "console_scripts": [
-            "targs = targs:main"
-        ]
-    }
+    install_requires=["wheel", "setuptools"],
+    py_modules=["targs"],
+    entry_points={"console_scripts": ["targs = targs:main"]},
 )
