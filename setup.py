41f6f85cfe0b465e5a6fce0a356d3f239da7a6d9
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-12-28 15:16:20 +0900

    setup.py: added dependency of wheel

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(
     name="targs",
     version="0.0.1",
+    install_requires=['wheel'],
     py_modules=['targs'],
     entry_points={
         "console_scripts": [
